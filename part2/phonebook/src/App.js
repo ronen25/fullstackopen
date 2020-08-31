152e61fb69931b250ea620f73851dea5859d1cb6
Ronen Lapushner <ronenk17@gmail.com>
2020-08-31 03:54:13 +0300

    2.9 step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,13 +5,18 @@ import Contact from './components/Contact';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]);
   const [newName, setNewName] = useState('');
   const [newPhone, setNewPhone] = useState('');
+  const [filter, setFilter] = useState('');
 
   const onNameChanged = (event) => setNewName(event.target.value);
   const onPhoneChanged = (event) => setNewPhone(event.target.value);
+  const onFilterChanged = (event) => setFilter(event.target.value);
 
   const resetInputs = () => {
     setNewName("");
@@ -36,7 +41,7 @@ const App = () => {
   
     const personDetails = {
       name: newName,
-      phone: newPhone
+      number: newPhone
     };
   
     setPersons(persons.concat(personDetails));
@@ -46,13 +51,19 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with
+        <input id="inputFilter" onChange={onFilterChanged} />
+      </div>
+
+      <h2>Add a new</h2>
       <form>
         <div>
-          name: <input id="inputName" onChangeCapture={onNameChanged}/>
+          name: <input id="inputName" onChange={onNameChanged}/>
         </div>
 
         <div>
-          phone: <input id="inputPhone" onChangeCapture={onPhoneChanged}/>
+          phone: <input id="inputPhone" onChange={onPhoneChanged}/>
         </div>
 
         <div>
@@ -61,7 +72,9 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {
-        persons.map(person => <Contact key={person.name} contact={person} />)
+        persons
+          .filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+          .map(person => <Contact key={person.name} contact={person} />)
       }
     </div>
   )
--- part2/phonebook/src/components/Contact.js
+++ part2/phonebook/src/components/Contact.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Contact = ({ contact }) => {
     return (
         <div>
-            {contact.name} {contact.phone}
+            {contact.name} {contact.number}
         </div>
     );
 };
