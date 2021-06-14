74923442bb9e8749fe7e2388cfa2c99866179acc
Ronen Lapushner <ronenk17@gmail.com>
2021-06-14 08:28:03 +0300

    Exercise 2.13 step 2

--- part2/countries/src/components/CountryDetails.js
+++ part2/countries/src/components/CountryDetails.js
@@ -1,6 +1,20 @@
 import CountryOverview from "./CountryOverview";
 
 const CountryDetails = ({ countries, error }) => {
+    const onShowButtonClicked = (event) => {
+        const index = event.target.id.split('_')[2];
+        const item_id = 'country_details_' + index;
+        const element = document.getElementById(item_id);
+
+        // Toggle display
+        if (element.style.display === 'inherit') {
+            element.style.display = 'none';
+        }
+        else {
+            element.style.display = 'inherit';
+        }
+    };
+
     // Error requires an error message to be rendered
     if (error !== '') {
         return <p style={{ background: 'maroon', color: 'white' }}> error </p>;
@@ -15,11 +29,20 @@ const CountryDetails = ({ countries, error }) => {
         );
     }
     else if (countries.length === 1) {
-        return <CountryOverview details={countries[0]} />;
+        return <CountryOverview details={ countries[0] } />;
     }
     else if (countries.length <= 10) {
         // Render a list of the countries names
-        let countries_list = countries.map((element, idx) => (<li key={idx}>{ element.name }</li>))
+        let countries_list = countries.map((country) => (
+            <li key={country.numericCode}>
+                { country.name } <span>&nbsp;&nbsp;</span>
+                <button id={ 'btn_show_' + country.numericCode } type='button'
+                        onClick={onShowButtonClicked}>Show</button>
+
+                <CountryOverview details={country} show={false} />
+            </li>
+        ));
+
         return (
             <ul>
             { countries_list }
--- part2/countries/src/components/CountryOverview.js
+++ part2/countries/src/components/CountryOverview.js
@@ -1,12 +1,13 @@
 import './css/CountryOverview.css';
 
-const CountryOverview = ({ details }) => {
+const CountryOverview = ({ details, show }) => {
     // Format population and languages
     const population = new Intl.NumberFormat().format(details.population);
     const languages_list = details.languages.map(lang => <li key={ lang.iso639_1 }> { lang.name }</li>);
 
     return (
-        <div class="country_overview">
+        <div id={ 'country_details_' + details.numericCode } 
+            className="country_overview" style={{ display: show ? 'inherit' : 'none' }}>
             <img src={ details.flag } alt='Country flag' align='top'></img>
 
             <div id="details">
